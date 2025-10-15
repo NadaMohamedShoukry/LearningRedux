fee349301a60d5a6b8398a510be24605c76cbc74
NadaMohamedShoukry <nadashoukry23@gmail.com>
2025-10-15 20:35:16 +0300

    learning redux without actio functions

---
+++ src/store.js
@@ -0,0 +1,54 @@
+import { createStore } from "redux";
+
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: 0,
+};
+
+//difference : here we pass the initial state as a default state.
+// Important rules for reducers:
+// Do not modify the existing state directly.
+// Avoid asynchronous logic or side effects.
+// Place as much logic as possible inside the reducer.
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    // write the actions in the shape of state domain.
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
+      //Later
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      // it is advised in redux to return the original state bake instead of
+      //throwing an error.
+      return state;
+  }
+}
+//create the store.
+const store = createStore(reducer);
+//dispatch actions
+store.dispatch({ type: "account/deposit", payload: 500 });
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 500, purpose: "Buy a car" },
+});
+
+console.log(store.getState());
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
