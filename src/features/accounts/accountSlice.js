b9eeb0651d6f13c5d93fc7fc76a7c85d2937c26b
NadaMohamedShoukry <nadashoukry23@gmail.com>
2025-10-20 18:39:50 +0300

    finished practising classic Redux

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,6 +2,7 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: 0,
+  isLoading: false,
 };
 //difference : here we pass the initial state as a default state.
 // Important rules for reducers:
@@ -12,7 +13,11 @@ export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     // write the actions in the shape of state domain.
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
@@ -31,14 +36,33 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       // it is advised in redux to return the original state bake instead of
       //throwing an error.
       return state;
   }
 }
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  //when redux sees that it will know that this function is a thunk
+  //it will execute it before dispatching to store
+  // Middleware.
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    //APi Call
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const converted = data.rates.USD;
+    console.log(converted);
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 
 export function withdraw(amount) {
